b52a388d9d80fac52567a817833180f40105f602
dimastr90 <34780847+dimastr90@users.noreply.github.com>
2019-12-10 19:50:24 +0200

    Update Missing letters.js

--- Intermediate Algorithm Scripting/Missing letters.js
+++ Intermediate Algorithm Scripting/Missing letters.js
@@ -1,6 +1,6 @@
 /* Find the missing letter in the passed letter range and return it.
 
-If all letters are present in the range, return undefined.   /*
+If all letters are present in the range, return undefined.   */
 
 function fearNotLetter(str) {
   let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
