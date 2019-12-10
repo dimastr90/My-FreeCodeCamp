2403b551b17d561607096fac7bdbf457d0411825
dimastr90 <34780847+dimastr90@users.noreply.github.com>
2019-12-10 19:49:51 +0200

    Create Missing letters.js

---
+++ Intermediate Algorithm Scripting/Missing letters.js
@@ -0,0 +1,16 @@
+/* Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.   /*
+
+function fearNotLetter(str) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  for(let i=0; i<str.length; i++){
+    if(alphabet[alphabet.indexOf(str.charAt(0))+i] !== str.charAt(i)){
+      return alphabet[alphabet.indexOf(str.charAt(0))+i];
+    }
+  }
+  return undefined;
+}
+
+fearNotLetter("abce");
