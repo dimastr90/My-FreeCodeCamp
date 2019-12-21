1868b0eac880a80421cce27f596697d2496ba915
dimastr90 <34780847+dimastr90@users.noreply.github.com>
2019-12-22 01:19:46 +0200

    Update Roman Numeral Converter.js

--- JavaScript Algorithms and Data Structures Projects/Roman Numeral Converter.js
+++ JavaScript Algorithms and Data Structures Projects/Roman Numeral Converter.js
@@ -4,7 +4,7 @@ All roman numerals answers should be provided in upper-case. */
 
 function convertToRoman(num) {
     let res = "";
-    let arrCompare = [[1000, 'M'],[900, 'CM'],[500, 'D'], [400, 'CD'], ['100', 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']];
+    let arrCompare = [[1000, 'M'],[900, 'CM'],[500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']];
     
     while(num!=0){
         for(let i=0; i<arrCompare.length; i++){
